88189578574f419e339241a4eec80541bb04e4e7
Mariol90 <mariolorenzo90@yahoo.com>
2022-03-23 19:56:16 -0400

    add helpers and payment test

---
+++ helpers.test.js
@@ -0,0 +1,42 @@
+describe('test 1er funcion',function(){
+    beforeEach(function(){
+        allPayments.payment1 = {
+            billAmt: 100,
+            tipAmt: 20,
+            tipPercent: 20
+          }
+        allPayments.payment2={
+            tipAmt: 20,
+            billAmt: 200,
+            tipPercent: 20,
+            }
+  
+        });
+        
+it("should calculate total from tip=40",function(){
+    expect(sumPaymentTotal('tipAmt')).toEqual(40);
+});
+it("should calculate total from bill=300",function(){
+    expect(sumPaymentTotal('billAmt')).toEqual(300);
+});
+it("should calculate total from tipPercent=40",function(){
+    expect(sumPaymentTotal('tipPercent')).toEqual(40);
+});
+afterEach(function(){
+    delete allPayments["payment1"];
+    delete allPayments["payment2"];
+})
+});
+
+
+describe("test 2do function", function(){
+    it("should calculate 100totalbill and 20tips",function(){
+    expect(calculateTipPercent(100,20)).toEqual(20);
+    });
+});
+// describe("test 3er function ",function(){
+
+// it ('create a table row and appends ',function(){
+//     expect();
+// })
+// });
