c77c786917e197f27fb41559ca642aa9d465786a
Mariol90 <mariolorenzo90@yahoo.com>
2022-03-24 13:31:15 -0400

    update on payment test

--- payments.test.js
+++ payments.test.js
@@ -1,19 +1,26 @@
 describe("1er funcion on payment",function(){
     beforeEach(function(){
-    
-        billAmtInput.value = '100';
-    tipAmtInput.value = '20';
+        allPayments.payment1 = {
+            billAmt: 100,
+            tipAmt: 20,
+        }
+   
     });
     it ('should add payment to allPayments in submitPaymentInfo', function(){
         submitPaymentInfo();
-        expect(allPayments['payment1'].billAmt).toEqual('100');
+        expect(allPayments['payment1'].billAmt).toEqual(100);
 });
     it ('should add tip to allPayments in submitPaymentInfo', function(){
         submitPaymentInfo();
-        expect(allPayments['payment1'].tipAmt).toEqual('20');
+        expect(allPayments['payment1'].tipAmt).toEqual(20);
 });
     it ('should add TipPercent to allPayments in submitPaymentInfo', function(){
         submitPaymentInfo();
         expect(Math.round(100/ (allPayments['payment1'].billAmt/allPayments['payment1'].tipAmt))).toEqual(20);
 });
+
+});
+afterEach(function(){
+    billAmtInput.value ='';
+    tipAmtInput.value ='';
 });
